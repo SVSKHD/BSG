473b027249b653979acb570446da9dce3b46f349
hithesh svsk <8svskhd@gmail.com>
2021-03-18 12:48:49 +0530

    seo changes2

--- src/components/seo.js
+++ src/components/seo.js
@@ -1,6 +1,5 @@
 import React from "react"
 import {Helmet} from "react-helmet"
-import ReactGA from "react-ga"
 const SEO = ({title , description , keywords , ogimg ,canurl , ogurl }) =>{
    
     return(
